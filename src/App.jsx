1bad6e9dad700c2d80fde2fbbc29b8bae4155808
Gona <kaiden@millie.town>
2026-06-06 13:25:58 +0900

    feat: remove pin lock, go directly to home

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,5 @@
-import { useState } from 'react'
-import LockScreen from './components/LockScreen/LockScreen'
 import AppShell from './components/AppShell/AppShell'
 
-const CORRECT_PIN = import.meta.env.VITE_APP_PIN || '123456'
-const SESSION_KEY = 'ledger_unlocked'
-
 export default function App() {
-  const [unlocked, setUnlocked] = useState(
-    () => sessionStorage.getItem(SESSION_KEY) === '1'
-  )
-
-  function handleUnlock() {
-    sessionStorage.setItem(SESSION_KEY, '1')
-    setUnlocked(true)
-  }
-
-  function handleLock() {
-    sessionStorage.removeItem(SESSION_KEY)
-    setUnlocked(false)
-  }
-
-  if (!unlocked) return <LockScreen correctPin={CORRECT_PIN} onUnlock={handleUnlock} />
-  return <AppShell onLock={handleLock} />
+  return <AppShell onLock={() => {}} />
 }
